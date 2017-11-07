913a4d6296f0a31bec375aef77819f5fe6a97ee7
xortiz <xavier.ortiz.ch@gmail.com>
2017-11-06 19:42:27 -0500

    cart created. Works. One issue that might need fixing. Need to remove cart item on click on the X. The item is removed from the cart, but the image of it isn't.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,8 @@ class BooksController < ApplicationController
 	# GET /books/1
 	# GET /books/1.json
 	def show
-
-    @book = Book.find(params[:id])
+		@book = Book.find(params[:id])
+		@cart_action = @book.cart_action current_user.try :id
 	end
 
 	#   # GET /books/new
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 	devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 	resources :books, only: [:show, :index]
 	resource :cart, only: [:show] do
-		put 'add/:movie_id', to: 'carts#add', as: :add_to
-		put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+		put 'add/:book_id', to: 'carts#add', as: :add_to
+		put 'remove/:book_id', to: 'carts#remove', as: :remove_from
 	end
 	# resources :books
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
